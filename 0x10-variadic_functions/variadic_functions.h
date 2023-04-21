877020cd7380acb5afc6271de94b59fa96f0c12b
CK-EmbPro <kennydebrice2@gmail.com>
2023-04-21 21:23:13 +0200

    variadic_functions header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef variadic_functions_h
-#define variadic_functions.h
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 #include<stdarg.h>
 
@@ -7,3 +7,5 @@ int sum = sum_them_all(const unsigned int n, ...);
 if(n==0){
 return 0;
 }
+
+#endif
